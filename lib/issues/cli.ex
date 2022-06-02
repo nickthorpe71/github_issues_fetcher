425171cd1dbaed35a33550986c332344dd03e652
Nick Thorpe <nickthorpe71@gmail.com>
2022-06-02 08:16:32 -0500

    add logic to be able to specify the number if issues received from the cli command

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -49,10 +49,17 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
     |> sort_into_decending_order()
+    |> last(count)
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse()
   end
 
   def sort_into_decending_order(list_of_issues) do
